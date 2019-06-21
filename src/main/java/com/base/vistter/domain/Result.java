3d134c91f612cd11a3de75f7b7622a3b36f037df
535081884@qq.com <lyl984510>
2019-06-21 11:25:54 +0800

    init

---
+++ src/main/java/com/base/vistter/domain/Result.java
@@ -0,0 +1,47 @@
+package com.base.vistter.domain;
+
+import java.io.Serializable;
+
+public class Result implements Serializable {
+
+    private static final long serialVersionUID = 5165102514695998105L;
+
+    private Integer code = 100;
+
+    private Object data = null;
+
+    public Result() {
+
+    }
+
+    public Result(Object data) {
+        this.data = data;
+    }
+
+    public static Result generErrorJson(Integer code) {
+        Result result = new Result();
+        result.setCode(code);
+        return result;
+    }
+
+    public static Result generJson(Object data) {
+        return new Result(data);
+    }
+
+    public Integer getCode() {
+        return code;
+    }
+
+    public void setCode(Integer code) {
+        this.code = code;
+    }
+
+    public Object getData() {
+        return data;
+    }
+
+    public void setData(Object data) {
+        this.data = data;
+    }
+
+}
---
+++ src/main/java/com/base/vistter/utils/Base64Utils.java
@@ -0,0 +1,21 @@
+package com.base.vistter.utils;
+
+import sun.misc.BASE64Encoder;
+
+import java.io.UnsupportedEncodingException;
+
+public class Base64Utils {
+
+    private static BASE64Encoder base64Encoder = new BASE64Encoder();
+
+    /**加密*/
+    public static String encode(String str){
+        try {
+            return base64Encoder.encode(str.getBytes("utf-8"));
+        }catch (UnsupportedEncodingException e){
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+}
